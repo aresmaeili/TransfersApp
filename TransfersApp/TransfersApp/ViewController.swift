86f77d18f984291781443960f5f5623b0d385f4f
AREM <aremiosdev@gmail.com>
2025-10-30 07:13:38 +0330

    Project Created!

---
+++ TransfersApp/TransfersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransfersApp
+//
+//  Created by AREM on 10/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
