eb9da8bbee2e02bf0d3c71cfba5f1c4680ecf7b0
AREM <aremiosdev@gmail.com>
2025-10-30 12:59:43 +0330

    Create all Colors may needed

---
+++ TransfersApp/Shared/Shared/Sources/Shared/Colors/Extension+UIColor.swift
@@ -0,0 +1,39 @@
+//
+//  Extension+UIColor.swift
+//  Shared
+//
+//  Created by AREM on 10/30/25.
+//
+
+import UIKit
+
+public extension UIColor {
+
+    // MARK: - Background
+    static var appBackground1: UIColor { UIAppColors.background1 }
+    static var appBackground3: UIColor { UIAppColors.background3 }
+    static var appBackground5: UIColor { UIAppColors.background5 }
+
+    // MARK: - Borders
+    static var appBorder1: UIColor { UIAppColors.border1 }
+    static var appBorder2: UIColor { UIAppColors.border2 }
+
+    // MARK: - Text
+    static var appText1: UIColor { UIAppColors.text1 }
+    static var appText2: UIColor { UIAppColors.text2 }
+    static var appText3: UIColor { UIAppColors.text3 }
+    static var appText6: UIColor { UIAppColors.text6 }
+    static var appText8: UIColor { UIAppColors.text8 }
+    static var appText9: UIColor { UIAppColors.text9 }
+    static var appText10: UIColor { UIAppColors.text10 }
+    static var appText11: UIColor { UIAppColors.text11 }
+    static var appText12: UIColor { UIAppColors.text12 }
+
+    // MARK: - Operator
+    static var appOperator1: UIColor { UIAppColors.operator1 }
+    static var appOperator2: UIColor { UIAppColors.operator2 }
+    static var appOperator3: UIColor { UIAppColors.operator3 }
+    static var appTransparent1: UIColor { UIAppColors.transparent1 }
+    static var appTransparent2: UIColor { UIAppColors.transparent2 }
+    static var appTransparent3: UIColor { UIAppColors.transparent3 }
+}
